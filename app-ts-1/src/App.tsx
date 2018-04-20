6f0ad8c3cde118d2cf0bf82131e27f16909b6458
Maya Malakova <maya.malakova@red-gate.com>
2018-04-20 14:03:07 +0100

    Remove intro

--- app-ts-1/src/App.tsx
+++ app-ts-1/src/App.tsx
@@ -18,11 +18,7 @@ class App extends Component {
                 <div className='App-header'>
                     <h2>Welcome Introduction to <code>web-kata#1-Typescript</code></h2>
                 </div>
-                <p className='App-intro'>
-                    To get started change this text and then save to reload.
-                </p>
                 <div className='products'>
-                    ... your code here
                     <ProductList products={data} />
                 </div>
             </div>);
